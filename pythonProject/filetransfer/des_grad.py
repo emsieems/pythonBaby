202552333daf4ed8268952f6ed8d3d6d02ff3e92
Fabella <emma.fabella@capgemini.com>
2022-10-20 15:02:30 +0800

    add new files

--- pythonProject/filetransfer/des_grad.py
+++ pythonProject/filetransfer/des_grad.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import time
 
+#first function
 def load_data(fname):
     points = np.loadtxt(fname, delimiter=',')
     y_ = points[:,1]
@@ -10,7 +11,10 @@ def load_data(fname):
     print('data here')
     return x_, y_
 
+#second function
 def evaluate_cost(x_,y_,params):
     tempcost = 0
     for i in range(len(y_)):
-        tempcost += (y_[i] - params)
+        tempcost += (y_[i] - (params[0] * x_[i,0]))
+        return tempcost / float(100)
+
